0305722cb868b3181ad5dd7caa24f9f8e3a31dc2
Rodriguez-Aleman, Daniel (ITD) <E323170@miamidade.gov>
2026-08-13 09:43:49 -0400

    supper power

---
+++ .superpowers/brainstorm/69-1786479132/start-companion.cmd
@@ -0,0 +1,6 @@
+@echo off
+set "BRAINSTORM_DIR=D:\Projects\mine\Slime_Isa\.superpowers\brainstorm\69-1786479132"
+set "BRAINSTORM_HOST=127.0.0.1"
+set "BRAINSTORM_URL_HOST=localhost"
+set "BRAINSTORM_PORT=57386"
+"C:\Users\e323170\.cache\codex-runtimes\codex-primary-runtime\dependencies\node\bin\node.exe" "C:\Users\e323170\.agents\skills\brainstorming\scripts\server.js"
